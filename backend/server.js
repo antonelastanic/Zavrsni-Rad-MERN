72194ff99f4c46793d473ee8277f54f511ba8eb9
unknown <antonela.stanic11@gmail.com>
2022-09-03 15:39:06 +0200

    Pokusaj izrade sustava za registraciju, authentikaciju i login(login ne radi trenutno)

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,29 @@
 const express = require('express');
+const app = express();
+
 const cors = require('cors');
 const mongoose = require('mongoose');
 
 require('dotenv').config();
-
-const app = express();
 const port = process.env.PORT || 5000;
+
+const pinRoute = require("./routes/pins");
+const userRoute = require("./routes/users");
 
 app.use(cors());
 app.use(express.json());
 
-
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true 
-    });
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true}
+    );
 
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("MongoDB database connection succesful!");
 });
 
+app.use("/api/pins", pinRoute);
+app.use("/api/users", userRoute);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
