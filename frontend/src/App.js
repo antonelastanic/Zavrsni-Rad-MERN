6d7996a96a993e9f642bdccbd0cdc32c86f30ee5
unknown <antonela.stanic11@gmail.com>
2022-09-07 01:54:20 +0200

    Implementirano dodavanje pinova na mapu(double click)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,34 +16,55 @@ function App() {
   const [currentPlaceId, setCurrentPlaceId] = useState(null);
   const [newPlace, setNewPlace] = useState(null);
 
+  const [title, setTitle] = useState(null);
+  const [desc, setDesc] = useState(null);
+  const [rating, setRating] = useState(0);
+
+
 
   useEffect(() => {
     const getPins = async () => {
-
       try{
         const res = await axios.get("/pins");
         setPins(res.data);
       } catch(err){
         console.log(err)
       }
-
     };
-
     getPins();
-
   },[]);
 
-  const handleMarkerClick = (id) => {
+  const handleMarkerClick = (id,lat,long) => {
     setCurrentPlaceId(id);
   }
 
-  const handleAddClick= (e) => {
+  const handleAddClick = (e) => {
     console.log(e);
-    const [lat, long] = e.lngLat;
+    const longitude = e.lngLat.lng
+    const latitude = e.lngLat.lat
     setNewPlace({
-      lat:lat,
-      long:long,
+      lat: latitude,
+      long: longitude,
     });
+  }
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+    const newPin = {
+      username: currentUser,
+      title,
+      desc,
+      rating,
+      lat: newPlace.lat,
+      long: newPlace.long,
+    }
+    try{
+        const res = await axios.post("/pins", newPin)
+        setPins([...pins, res.data])
+        setNewPlace(null);
+    } catch(err){
+        console.log(err);
+    }
   }
 
 
@@ -67,9 +88,10 @@ function App() {
     <Marker longitude={p.long} latitude={p.lat} anchor="bottom" >
       <Room 
       style = {{color: p.username === currentUser ? "slateblue" : "tomato", cursor: "pointer"}}
-      onClick = {() => handleMarkerClick(p._id)}
+      onClick = {() => handleMarkerClick(p._id,p.lat,p.log)}
       />
     </Marker>
+
     {p._id === currentPlaceId && (
     <Popup 
       longitude={p.long} 
@@ -85,11 +107,7 @@ function App() {
           <label>Rating</label>
           <div className="stars">
 
-          <Star className="star"/>
-          <Star className="star"/>
-          <Star className="star"/>
-          <Star className="star"/>
-          <Star className="star"/>
+          {Array(p.rating).fill(<Star className="star"/>)}
 
         </div>
           <label>Information</label>
@@ -100,17 +118,36 @@ function App() {
     )}
 </>
       ))}
+
       {newPlace && (
+
       <Popup 
         latitude={newPlace.lat}
         longitude={newPlace.long} 
         closeButton = {true}
         closeOnClick = {false}
         anchor="left"
-        onClose={() => setCurrentPlaceId(null)}
+        onClose={() => setNewPlace(null)}
       >
-        hello
+        <div>
+          <form onSubmit={handleSubmit}>
+            <label>Title</label>
+            <input placeholder="Enter a title.." onChange={(e) => setTitle(e.target.value)}/>
+            <label>Review</label>
+            <textarea placeholder="Write something about this place..." onChange={(e) => setDesc(e.target.value)}/>
+            <label>Rating</label>
+            <select onChange={(e) => setRating(e.target.value)}>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+            <button className="submitButton">Add Pin</button>
+          </form>
+        </div>
       </Popup>
+
     )}
     </Map>
   );
